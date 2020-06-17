496c7a2d8bb21e5589e9674a364f970b8b7bdce3
lenny <lennysamz@gmail.com>
2020-06-17 23:20:13 +0800

    First draft prototype

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from Insta.models import Post, InstaUser, Comment, Like
+from Insta.models import Post, InstaUser, Comment, Like, UserConnection
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(InstaUser)
 admin.site.register(Like)
 admin.site.register(Comment)
+admin.site.register(UserConnection)
 
 
--- Insta/views.py
+++ Insta/views.py
@@ -1,5 +1,6 @@
+from annoying.decorators import ajax_request
 from django.views.generic import TemplateView, ListView, DetailView
-from Insta.models import Post
+from Insta.models import Post, Like, InstaUser, UserConnection
 from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.urls import reverse, reverse_lazy
 
@@ -14,9 +15,20 @@ class PostsView(ListView):
     model=Post
     template_name='index.html'
 
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
+
 class PostDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
+
+class UserDetailView(DetailView):
+    model=InstaUser
+    template_name='user_detail.html'
 
 class PostCreateView(LoginRequiredMixin,CreateView):
     model=Post
@@ -38,6 +50,24 @@ class SignUp(CreateView):
     form_class=CustomUserCreationForm
     template_name='signup.html'
     success_url=reverse_lazy("login")
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
 
 
 
